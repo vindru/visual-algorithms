33d32fd4d6e83bd2f183b09826fd838919ae9ed3
rajatvindr00@gmail.com <rajatvindr00@gmail.com>
2021-01-04 16:08:44 +0530

    Updates

---
+++ precache-manifest.b06db105606708e6783fc1c792c76422.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092fbaea98b2ea9124672d9a074e9d3d",
+    "url": "/vindru/visual-algorithms/index.html"
+  },
+  {
+    "revision": "f24ec8789434580a23e5",
+    "url": "/vindru/visual-algorithms/static/css/2.5d068c69.chunk.css"
+  },
+  {
+    "revision": "b83ea8cc422061342dba",
+    "url": "/vindru/visual-algorithms/static/css/main.46e15791.chunk.css"
+  },
+  {
+    "revision": "f24ec8789434580a23e5",
+    "url": "/vindru/visual-algorithms/static/js/2.6f03d5d0.chunk.js"
+  },
+  {
+    "revision": "9f81f95fdf77198dcf92d14b566e110e",
+    "url": "/vindru/visual-algorithms/static/js/2.6f03d5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83ea8cc422061342dba",
+    "url": "/vindru/visual-algorithms/static/js/main.e636c4b4.chunk.js"
+  },
+  {
+    "revision": "f152d88c9d5cb3bb8b4d",
+    "url": "/vindru/visual-algorithms/static/js/runtime-main.d379923a.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/vindru/visual-algorithms/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/vindru/visual-algorithms/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/vindru/visual-algorithms/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/vindru/visual-algorithms/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/vindru/visual-algorithms/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "8cf49906e413c11d58bcf7bc8bc9370f",
+    "url": "/vindru/visual-algorithms/static/media/destination.8cf49906.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/vindru/visual-algorithms/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/vindru/visual-algorithms/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/vindru/visual-algorithms/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/vindru/visual-algorithms/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/vindru/visual-algorithms/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/vindru/visual-algorithms/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "94db7e92e51707073eea9d740f69e975",
+    "url": "/vindru/visual-algorithms/static/media/linkedinBlue.94db7e92.png"
+  },
+  {
+    "revision": "ec14de7c57c7a2815c413d6391802ca1",
+    "url": "/vindru/visual-algorithms/static/media/logo.ec14de7c.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/vindru/visual-algorithms/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/vindru/visual-algorithms/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/vindru/visual-algorithms/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/vindru/visual-algorithms/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/vindru/visual-algorithms/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "30235091909ad48c1fd850ff8c0a62de",
+    "url": "/vindru/visual-algorithms/static/media/start.30235091.png"
+  },
+  {
+    "revision": "4508852c7216336b1254c2dc297eb5f9",
+    "url": "/vindru/visual-algorithms/static/media/weight.4508852c.png"
+  }
+]);
